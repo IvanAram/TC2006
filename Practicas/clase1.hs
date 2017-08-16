33a85cefc0920e5affdc5d9710cfedd53fee117a
IvanAram <a01022584@itesm.mx>
2017-08-16 13:58:39 -0500

    Ejercicios clase 1 commit 2

--- Practicas/clase1.hs
+++ Practicas/clase1.hs
@@ -1,30 +1,61 @@
 import Data.Char
--- EJERCICIO 1 (z^2 == x^2 + y^2)
---TODOS LOS NUMEROS (INCLUYENDO DECIMALES)
+
+--		EJERCICIO 1 (z^2 == x^2 + y^2)
+--		TODOS LOS NUMEROS (INCLUYENDO DECIMALES)
 [(x,y,z) | x <- [1..100], y <- [1..100], let z = sqrt(x^2 + y^2)]
 
---TODOS LOS NUMEROS (EXCLUYENDO DECIMALES)
+--		TODOS LOS NUMEROS (EXCLUYENDO DECIMALES)
 [(x,y,z) | x <- [1..100], y <- [1..100], z <- [1..100], z^2 == x^2 + y^2]
 
 
 
--- EJERCICIO 2 (contar numeros negativos)
---RESPUESTA 1
+
+-- 		EJERCICIO 2 (contar numeros negativos)
+--		RESPUESTA 1
 length [a | x <- [-1000..1000], x < 0]
 
---RESPUESTA 2
+--		RESPUESTA 2
 length(filter(<0) [-1000..1000])
 
 
 
--- EJERCICIO 3 (suma numeros impares)
+
+--		EJERCICIO 3 (suma numeros impares)
 sum [x | x <- [1..1000000], mod x 2 == 1]
 
 
 
--- EJERCICIO 4 (texto de minusculas a mayusculas)
---CON MAP
-map(toUpper)[x | x <- "abcdefghijklmnopqrstuvwxyz"]
 
---SIN MAP
+--		EJERCICIO 4 (texto de minusculas a mayusculas)
+--		CON MAP
+map(toUpper)[x | x <- "abcdefghijklmnopqrstuvwxyz"] -- ESTO SE PUEDE HACER SIN LIST COMPRESSION
+
+--		SIN MAP
 [y | x <- "abcdefghijklmnopqrstuvwxyz", let y = toUpper x]
+
+--		CON FUNCION
+mFunc y = [toUpper x | x <- y]
+
+
+
+
+--		EJERCICIO 5 (recursividad)
+maxlist [] = error "Error"
+maxlist [e] = e
+maxlist (h:t) = max h (maxlist t) 
+
+
+
+
+--		EJERCICIO 6 (factorial)
+factorial 0 = 1
+factorial 1 = 1
+factorial n = n * factorial (n-1)
+
+
+
+
+--		EJERCICIO 7 (ultimos 3 numeros de una lista)
+lasttt a [] cnt = error "Error"
+lasttt 0 b cnt = []
+lasttt a (h:t) cnt = 
